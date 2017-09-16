41854de6aa173b958f1e592126f4a1e390fd5f28
Anna Isakova <tamgdepesok666@gmail.com>
2017-09-16 22:44:12 +0300

    created customer entity

---
+++ src/main/java/me/annaisakova/customers/entities/Customer.java
@@ -0,0 +1,85 @@
+package me.annaisakova.customers.entities;
+
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    private String phone;
+
+    public Customer() {
+    }
+
+    public Customer(String name, String surname, String phone) {
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Customer customer = (Customer) o;
+
+        if (id != customer.id) return false;
+        if (!name.equals(customer.name)) return false;
+        if (surname != null ? !surname.equals(customer.surname) : customer.surname != null) return false;
+        return phone.equals(customer.phone);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + name.hashCode();
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + phone.hashCode();
+        return result;
+    }
+}
